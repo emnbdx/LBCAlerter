57bce958133dc2fcb6e6aee3ade4228788260bc9
Eddy MONTUS <eddy.montus@gmail.com>
2014-10-25 11:54:40 +0200

    LBCAlerter finish paypal implementation

--- LBCAlerterWeb/Controllers/DonController.cs
+++ LBCAlerterWeb/Controllers/DonController.cs
@@ -1,12 +1,24 @@
 ﻿
 namespace LBCAlerterWeb.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
     using System.Data.Entity;
+    using System.IO;
+    using System.Linq;
     using System.Net;
+    using System.Text;
     using System.Threading.Tasks;
     using System.Web.Mvc;
 
     using LBCAlerterWeb.Models;
+
+    using log4net;
+
+    using WebGrease;
+
+    using LogManager = log4net.LogManager;
 
     /// <summary>
     /// The payment controller.
@@ -14,6 +26,11 @@ namespace LBCAlerterWeb.Controllers
     [Authorize]
     public class DonController : Controller
     {
+        /// <summary>
+        /// The log.
+        /// </summary>
+        private static readonly ILog Log = LogManager.GetLogger(typeof(DonController));
+
         /// <summary>
         /// The db.
         /// </summary>
@@ -67,30 +84,125 @@ namespace LBCAlerterWeb.Controllers
         {
             return this.View();
         }
-        
+
         /// <summary>
-        /// POST: Payment/Create
-        /// Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
-        /// plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// The merci.
         /// </summary>
-        /// <param name="payment">
-        /// The payment.
-        /// </param>
         /// <returns>
-        /// The <see cref="Task"/>.
+        /// The <see cref="ActionResult"/>.
         /// </returns>
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "ID,PaypalId,CreationDate,UpdateDate,State,Amount,Currency")] Don payment)
+        public ActionResult Merci()
         {
-            if (!this.ModelState.IsValid)
+            return this.View();
+        }
+
+        /// <summary>
+        /// The pay pal payment notification.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        [AllowAnonymous]
+        public ActionResult PayPalPaymentNotification()
+        {
+            Log.Debug("New IPN notification...");
+            
+            // Receive IPN request from PayPal and parse all the variables returned
+            var formVals = new Dictionary<string, string> { { "cmd", "_notify-validate" } };
+
+            // if you want to use the PayPal sandbox change this from false to true
+            var response = GetPayPalResponse(formVals, ConfigurationManager.AppSettings["paypalMode"] != "prod");
+
+            if (response != "VERIFIED")
             {
-                return this.View(payment);
+                return this.View();
             }
 
-            this.db.Dons.Add(payment);
-            await this.db.SaveChangesAsync();
-            return this.RedirectToAction("Index");
+            var id = this.Request["txn_id"];
+            var stringAmount = this.Request["mc_gross"];
+            var currency = this.Request["mmc_currency"];
+            var state = this.Request["payment_status"];
+            var email = this.Request["payer_email"];
+
+            var user = this.db.Users.FirstOrDefault(entry => entry.UserName == email);
+
+            // validate the order
+            decimal amount = 0;
+            decimal.TryParse(stringAmount, out amount);
+
+            var don = new Don
+                          {
+                              PaypalId = id,
+                              CreationDate = DateTime.Now,
+                              UpdateDate = DateTime.Now,
+                              State = state,
+                              Amount = amount,
+                              Currency = currency,
+                              User = user
+                          };
+
+            this.db.Dons.Add(don);
+            this.db.SaveChanges();
+
+            return this.View();
+        }
+
+        /// <summary>
+        /// The get pay pal response.
+        /// </summary>
+        /// <param name="formVals">
+        /// The form vals.
+        /// </param>
+        /// <param name="useSandbox">
+        /// The use sandbox.
+        /// </param>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        private string GetPayPalResponse(Dictionary<string, string> formVals, bool useSandbox)
+        {
+            // Parse the variables
+            // Choose whether to use sandbox or live environment
+            var paypalUrl = useSandbox
+                                   ? "https://www.sandbox.paypal.com/cgi-bin/webscr"
+                                   : "https://www.paypal.com/cgi-bin/webscr";
+
+            var req = (HttpWebRequest)WebRequest.Create(paypalUrl);
+
+            // Set values for the request back
+            req.Method = "POST";
+            req.ContentType = "application/x-www-form-urlencoded";
+
+            var param = Request.BinaryRead(Request.ContentLength);
+            var strRequest = Encoding.ASCII.GetString(param);
+
+            var sb = new StringBuilder();
+            sb.Append(strRequest);
+
+            foreach (var key in formVals.Keys)
+            {
+                sb.AppendFormat("&{0}={1}", key, formVals[key]);
+            }
+
+            strRequest += sb.ToString();
+            req.ContentLength = strRequest.Length;
+
+            // for proxy
+            // WebProxy proxy = new WebProxy(new Uri("http://urlort#");
+            // req.Proxy = proxy;
+            // Send the request to PayPal and get the response
+            string response;
+            using (var streamOut = new StreamWriter(req.GetRequestStream(), Encoding.ASCII))
+            {
+                streamOut.Write(strRequest);
+                streamOut.Close();
+                using (var streamIn = new StreamReader(req.GetResponse().GetResponseStream()))
+                {
+                    response = streamIn.ReadToEnd();
+                }
+            }
+
+            return response;
         }
 
         /// <summary>
--- LBCAlerterWeb/Global.asax.cs
+++ LBCAlerterWeb/Global.asax.cs
@@ -1,19 +1,22 @@
-﻿using LBCAlerterWeb.Models;
-using log4net;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
-
+﻿
 namespace LBCAlerterWeb
 {
+    using System.Configuration;
+    using System.Data.Entity;
+    using System.Web.Mvc;
+    using System.Web.Optimization;
+    using System.Web.Routing;
+
+    using LBCAlerterWeb.Models;
+
+    /// <summary>
+    /// The mvc application.
+    /// </summary>
     public class MvcApplication : System.Web.HttpApplication
     {
+        /// <summary>
+        /// The application_ start.
+        /// </summary>
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -21,13 +24,17 @@ namespace LBCAlerterWeb
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            //Init log
+            // Init log
             log4net.Config.XmlConfigurator.Configure();
 
-            if(ConfigurationManager.AppSettings["dbMode"] == "debug")
+            if (ConfigurationManager.AppSettings["dbMode"] == "debug")
+            {
                 Database.SetInitializer<ApplicationDbContext>(new DebugInitializer());
+            }
             else
+            {
                 Database.SetInitializer<ApplicationDbContext>(new ProductionInitializer());
+            }
         }
     }
 }
