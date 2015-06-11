92d4a62b83368f8b83b9998928149614da862bb8
Eddy MONTUS <eddy.montus@gmail.com>
2015-06-11 15:33:57 +0200

    lbcalerter ajout de la vidéo de tuto

---
+++ LBCAlerterWeb/Views/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace LBCAlerterWeb.Views
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult HowItsWork()
+        {
+            return View();
+        }
+    }
+}
