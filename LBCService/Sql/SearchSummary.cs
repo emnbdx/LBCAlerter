93e08ec51066c1598c9173e65cd9c2d613c3b664
Eddy MONTUS <eddy.montus@gmail.com>
2015-12-19 03:03:31 +0100

    add exclude directory

---
+++ LBCService/Sql/SearchSummary.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace LBCService.Sql
+{
+    public class SearchSummary
+    {
+        public int Id { get; set; }
+
+        public string KeyWord { get; set; }
+
+        public string Url { get; set; }
+
+        public int RefreshTime { get; set; }
+
+        public string UserName { get; set; }
+
+        public bool IsPremiumUser { get; set; }
+
+        public bool Enabled { get; set; }
+
+        public bool MailAlert { get; set; }
+
+        public bool MailRecap { get; set; }
+
+        public DateTime? LastRecap { get; set; }
+
+        public int TodayAttempsCount { get; set; }
+
+        public int AdsCount { get; set; }
+
+        public int TodayAdsCount { get; set; }
+
+        public int FirstAdId { get; set; }
+    }
+}
